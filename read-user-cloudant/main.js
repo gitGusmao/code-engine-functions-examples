37e2f46669b0dab7d81fdb90a4600656be5c22c5
unknown <Leonardo>
2023-11-19 21:55:43 -0300

    teste

--- read-user-cloudant/main.js
+++ read-user-cloudant/main.js
@@ -1,14 +1,39 @@
-function main(args) {
-
+async function main(args) {
   const { CloudantV1 } = require("@ibm-cloud/cloudant");
+
+  const client = CloudantV1.newInstance();
 
   const dbName = args.dbName;
   const document = args.document;
 
+  if (dbName == undefined || document == undefined)
+    return res.status(404).send({
+      message: "Error",
+    });
+
+  let response;
+
+  await client
+    .postFind({
+      db: dbName,
+      limit: 1,
+      selector: {
+        document: {
+          $eq: document,
+        },
+      },
+    })
+    .then((res) => {
+      res = res.result;
+      if (res.docs.length > 0) response = res.docs[0];
+    })
+    .catch((err) => {
+      response = err.error;
+    });
 
   return {
-    headers: { "Content-Type": "text/plain;charset=utf-8" },
-    body: dbName,
+    headers: { "Content-Type": "application/json;charset=utf-8" },
+    body: response,
   };
 }
 

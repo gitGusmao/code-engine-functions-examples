70dee328041956023268e62c63a8f6ffcb8aab87
unknown <Leonardo>
2023-11-19 21:14:23 -0300

    teste

--- read-user-cloudant/read-user-cloudant.js
+++ read-user-cloudant/read-user-cloudant.js
@@ -1,49 +1,11 @@
-// const { CloudantV1 } = require("@ibm-cloud/cloudant");
-
-// require("dotenv").config();
-
-// const client = CloudantV1.newInstance();
-
-async function main(params) {
-  console.log(params);
-  console.log("i");
-  // const jsonObject = JSON.parse(jsonString);
-
-  // const dbName = jsonObject["dbName"];
-  // const document = jsonObject["document"];
-
-  // console.log(dbName, document);
-
-  // if (dbName == undefined || document == undefined)
-  //   return res.status(404).send({
-  //     message: "Error",
-  //   });
-
-  // let response;
-
-  // await client
-  //   .postFind({
-  //     db: dbName,
-  //     limit: 1,
-  //     selector: {
-  //       document: {
-  //         $eq: document,
-  //       },
-  //     },
-  //   })
-  //   .then((res) => {
-  //     res = res.result;
-  //     if (res.docs.length > 0) response = res.docs[0];
-  //   })
-  //   .catch((err) => {
-  //     response = err.error;
-  //   });
-
-  // console.log(response);
+function main(args) {
+  const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+  const lorem = new LoremIpsum();
 
   return {
     headers: { "Content-Type": "text/plain;charset=utf-8" },
-    body: params,
+    body: lorem.generateWords(10),
   };
 }
+
 module.exports.main = main;

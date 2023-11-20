d86a8f53342968f019ba07d90b42252130ac500d
unknown <Leonardo>
2023-11-19 22:10:03 -0300

    teste

--- read-user-cloudant/main.js
+++ read-user-cloudant/main.js
@@ -1,39 +1,39 @@
 async function main(args) {
-  const { CloudantV1 } = require("@ibm-cloud/cloudant");
+  // const { CloudantV1 } = require("@ibm-cloud/cloudant");
 
-  const client = CloudantV1.newInstance();
+  // const client = CloudantV1.newInstance();
 
   const dbName = args.dbName;
   const document = args.document;
 
-  if (dbName == undefined || document == undefined)
-    return res.status(404).send({
-      message: "Error",
-    });
+  // if (dbName == undefined || document == undefined)
+  //   return res.status(404).send({
+  //     message: "Error",
+  //   });
 
-  let response;
+  // let response;
 
-  await client
-    .postFind({
-      db: dbName,
-      limit: 1,
-      selector: {
-        document: {
-          $eq: document,
-        },
-      },
-    })
-    .then((res) => {
-      res = res.result;
-      if (res.docs.length > 0) response = res.docs[0];
-    })
-    .catch((err) => {
-      response = err.error;
-    });
+  // await client
+  //   .postFind({
+  //     db: dbName,
+  //     limit: 1,
+  //     selector: {
+  //       document: {
+  //         $eq: document,
+  //       },
+  //     },
+  //   })
+  //   .then((res) => {
+  //     res = res.result;
+  //     if (res.docs.length > 0) response = res.docs[0];
+  //   })
+  //   .catch((err) => {
+  //     response = err.error;
+  //   });
 
   return {
     headers: { "Content-Type": "application/json;charset=utf-8" },
-    body: response,
+    body: dbName + document,
   };
 }
 

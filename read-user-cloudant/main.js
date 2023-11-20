a4c6e87ce7391c6e179150e7c2f94ac2050bdc4d
unknown <Leonardo>
2023-11-19 21:47:42 -0300

    teste

--- read-user-cloudant/main.js
+++ read-user-cloudant/main.js
@@ -1,10 +1,14 @@
 function main(args) {
-  const LoremIpsum = require("lorem-ipsum").LoremIpsum;
-  const lorem = new LoremIpsum();
+
+  const { CloudantV1 } = require("@ibm-cloud/cloudant");
+
+  const dbName = args.dbName;
+  const document = args.document;
+
 
   return {
     headers: { "Content-Type": "text/plain;charset=utf-8" },
-    body: args,
+    body: dbName,
   };
 }
 
